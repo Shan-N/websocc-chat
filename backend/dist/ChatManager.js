a9a7fb2fdfd9290e3948794a3bd550ef7809c106
Shan-N <shantanu.neve2000@gmail.com>
2025-02-10 10:12:27 +0530

    added roomId

--- backend/dist/ChatManager.js
+++ backend/dist/ChatManager.js
@@ -2,40 +2,87 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatManager = void 0;
 const Chat_1 = require("./Chat");
+const uuid_1 = require("uuid");
 const message_1 = require("./message");
 class ChatManager {
     constructor() {
-        this.chats = [];
-        this.users = [];
-        this.pendingUser = null;
+        this.chats = new Map();
+        this.users = new Map();
     }
     addUser(socket) {
-        this.users.push(socket);
+        this.users.set(socket, ""); // No room assigned yet
         this.addHandler(socket);
     }
     removeUser(socket) {
-        this.users = this.users.filter(s => s !== socket);
+        const roomId = this.users.get(socket);
+        if (roomId) {
+            const chat = this.chats.get(roomId);
+            if (chat) {
+                if (chat.user1 === socket) {
+                    chat.user1 = chat.user2;
+                    chat.user2 = null;
+                }
+                else if (chat.user2 === socket) {
+                    chat.user2 = null;
+                }
+                if (!chat.user1 && !chat.user2) {
+                    this.chats.delete(roomId);
+                }
+            }
+        }
+        this.users.delete(socket);
     }
     addHandler(socket) {
-        socket.on('message', (data) => {
+        socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
             if (message.type === message_1.INIT) {
-                if (this.pendingUser) {
-                    const chat = new Chat_1.Chat(this.pendingUser, socket);
-                    this.chats.push(chat);
-                    this.pendingUser = null;
+                if (message.roomId) {
+                    this.joinRoom(socket, message.roomId);
                 }
                 else {
-                    this.pendingUser = socket;
+                    this.createRoom(socket);
                 }
             }
             if (message.type === message_1.CHAT_LINES) {
-                const chat = this.chats.find(chat => chat.user1 === socket || chat.user2 === socket);
-                if (chat) {
-                    chat.addMessage(socket, message.line);
+                const roomId = this.users.get(socket);
+                if (roomId) {
+                    const chat = this.chats.get(roomId);
+                    if (chat) {
+                        chat.addMessage(socket, message.line);
+                        console.log(message.line);
+                    }
                 }
             }
         });
+        socket.on("close", () => {
+            this.removeUser(socket);
+        });
+    }
+    createRoom(socket) {
+        const roomId = (0, uuid_1.v4)();
+        const chat = new Chat_1.Chat(socket);
+        this.chats.set(roomId, chat);
+        this.users.set(socket, roomId);
+        socket.send(JSON.stringify({
+            type: "ROOM_CREATED",
+            roomId: roomId,
+        }));
+    }
+    joinRoom(socket, roomId) {
+        const chat = this.chats.get(roomId);
+        if (chat && chat.addUser(socket)) {
+            this.users.set(socket, roomId);
+            socket.send(JSON.stringify({
+                type: "ROOM_JOINED",
+                roomId: roomId,
+            }));
+        }
+        else {
+            socket.send(JSON.stringify({
+                type: "ERROR",
+                message: "Room not found or full.",
+            }));
+        }
     }
 }
 exports.ChatManager = ChatManager;
--- backend/src/ChatManager.ts
+++ backend/src/ChatManager.ts
@@ -1,48 +1,101 @@
 import { WebSocket } from "ws";
 import { Chat } from "./Chat";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 import { CHAT_LINES, INIT } from "./message";
 
-
-
 export class ChatManager {
-    private chats : Chat[];
-    private users : WebSocket[];
-    private pendingUser : WebSocket | null;
+    private chats: Map<string, Chat>; 
+    private users: Map<WebSocket, string>;
+
     constructor() {
-        this.chats = [];
-        this.users = [];
-        this.pendingUser = null;
+        this.chats = new Map();
+        this.users = new Map();
     }
 
-    addUser(socket : WebSocket){
-        this.users.push(socket);
+    addUser(socket: WebSocket) {
+        this.users.set(socket, ""); // No room assigned yet
         this.addHandler(socket);
     }
-    
-    removeUser(socket : WebSocket){
-        this.users = this.users.filter(s => s!== socket);
+
+    removeUser(socket: WebSocket) {
+        const roomId = this.users.get(socket);
+        if (roomId) {
+            const chat = this.chats.get(roomId);
+            if (chat) {
+                if (chat.user1 === socket) {
+                    chat.user1 = chat.user2;
+                    chat.user2 = null;
+                } else if (chat.user2 === socket) {
+                    chat.user2 = null;
+                }
+
+                if (!chat.user1 && !chat.user2) {
+                    this.chats.delete(roomId); 
+                }
+            }
+        }
+        this.users.delete(socket);
     }
 
-    private addHandler(socket : WebSocket){
-        socket.on('message', (data) => {
+    private addHandler(socket: WebSocket) {
+        socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-            if(message.type === INIT) {
-                if(this.pendingUser){
-                    const chat = new Chat(this.pendingUser, socket);
-                    this.chats.push(chat);
-                    this.pendingUser = null;
-                }
-                else{
-                    this.pendingUser = socket;
+
+            if (message.type === INIT) {
+                if (message.roomId) {
+                    this.joinRoom(socket, message.roomId);
+                } else {
+                    this.createRoom(socket);
                 }
             }
-            if(message.type === CHAT_LINES){
-                const chat = this.chats.find(chat => chat.user1 === socket || chat.user2 === socket);
-                if(chat){
-                    chat.addMessage(socket, message.line);
+
+            if (message.type === CHAT_LINES) {
+                const roomId = this.users.get(socket);
+                if (roomId) {
+                    const chat = this.chats.get(roomId);
+                    if (chat) {
+                        chat.addMessage(socket, message.line);
+                    }
                 }
             }
-        })
+        });
+
+        socket.on("close", () => {
+            this.removeUser(socket);
+        });
+    }
+
+    private createRoom(socket: WebSocket) {
+        const roomId = uuidv4();
+        const chat = new Chat(socket);
+        this.chats.set(roomId, chat);
+        this.users.set(socket, roomId);
+
+        socket.send(
+            JSON.stringify({
+                type: "ROOM_CREATED",
+                roomId: roomId,
+            })
+        );
+    }
+
+    private joinRoom(socket: WebSocket, roomId: string) {
+        const chat = this.chats.get(roomId);
+        if (chat && chat.addUser(socket)) {
+            this.users.set(socket, roomId);
+            socket.send(
+                JSON.stringify({
+                    type: "ROOM_JOINED",
+                    roomId: roomId,
+                })
+            );
+        } else {
+            socket.send(
+                JSON.stringify({
+                    type: "ERROR",
+                    message: "Room not found or full.",
+                })
+            );
+        }
     }
 }
